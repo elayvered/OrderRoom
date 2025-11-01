93bd897fab5e40bb7f809b2cc539de75168fdf9d
elayvered <elayvered@gmail.com>
2025-11-01 16:43:39 +0200

    Update or.config.js

--- or.config.js
+++ or.config.js
@@ -1,6 +1,4 @@
-<script>
 window.OR_CONFIG = {
-  SUPABASE_URL: 'https://YOUR-REF.supabase.co',
-  SUPABASE_ANON: 'YOUR-ANON-PUBLIC-KEY'
-}
-</script>
+  SUPABASE_URL: 'https://vquzmatigijeevwxowkp.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxdXptYXRpZ2lqZWV2d3hvd2twIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NjcyOTUsImV4cCI6MjA3NzU0MzI5NX0.a4wy2K5pPm699Dy92MZ2h8W9r3tSrQaJuuZE-b978qw'
+};
